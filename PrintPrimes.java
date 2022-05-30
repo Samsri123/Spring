dd078f67c559aa3cc7c9de6131c20aaafb21f9f8
srikanth <srikanthc43@gmail.com>
2022-05-30 04:15:19 +0000

    error correction

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -5,16 +5,16 @@ class PrintPrimes {
   	Scanner s =  new Scanner(System.in);
 	System.out.println("enter starting number");
 	int n1 = s.nextInt();
-	System.out.println("enter ending number")'
+	System.out.println("enter ending number");
 	int n2 = s.nextInt();
 	System.out.println("Primes numbers from " + n1 + " to " + n2 + " are:");
 	for (int i = n1; i <= n2; i++) {
 	   int count = 0;
-		for (int j = 1; j <= n1; j++) {
-		   if ( n1 % j == 0)
+		for (int j = 1; j <= i; j++) {
+		   if ( i % j == 0)
                        count++;
 		}
-	   if (count ==2)
+	   if (count == 2)
               System.out.print(i+" ");
 	}
     }
